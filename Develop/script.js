d41f46492d2dec228410461060a96261884a6eba
Sweat <upsidedownplatypus@gmail.com>
2020-06-25 23:39:45 -0400

    cleaned up JS, styling

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,21 @@
 $( document ).ready(function() {
 
-    var container = $('.container');
     var currentTime = moment().format('LT');
+    var date = moment().calendar();
     var currentHour = parseInt(currentTime[0]+currentTime[1]);
     var upcoming = false;
+    var container = $('.container');
 
-    var date = moment().calendar();
+    //Places date in Jumbotron
     $("#currentDay").append(date);
 
-    var storage = JSON.parse(localStorage.getItem('planner'));
-    if (storage === null){
-        storage = {
-            "9AM" : "", "10AM" : "","11AM" : "","12PM" : "","1PM" : "","2PM" : "","3PM" : "","4PM" : "","5PM" : ""
-        }
-    }
-    console.log(storage)
-    console.log(currentTime[0])
-    console.log(currentTime[1])
-    console.log(currentTime[2])
-    console.log(currentTime[3])
-    console.log(currentTime[4])
-    console.log(currentTime[5])
-    console.log(currentTime[6])
-
+    //Function to create rows and labels for each time block
     function createHourRow(time, data){
         var displayTime = time;
         var row = $('<div>').attr('id', time).addClass('row hour-block');
         container.append(row);
 
+        //For loop executes creating columns in each row
         for(var i = 0; i < 3; i++){
             var newCol = $('<div>');
             switch(i){
@@ -35,32 +23,45 @@ $( document ).ready(function() {
                     newCol.addClass('col-md-2 hour').text(displayTime);
                     break;
                 case 1:
-                    newCol.addClass('col-md-8');
                     var inputArea = $('<textarea>').text(data).addClass('event');
-                    newCol.append(inputArea);
+                    newCol.append(inputArea).addClass('col-md-8');
                     break;
                 case 2:
                     newCol.addClass('col-md-2 saveTime').text("Save Event");
                     break;
             }
-            row.append(newCol).addClass('past');
+            //Compares current times and applies the appropriate class
+            row.append(newCol);
             if(upcoming && parseInt(time) !== currentHour){
                 row.addClass('upcoming');
-                row.removeClass('past');
             }
-            else if(parseInt(time) === currentHour){
+            if(parseInt(time) !== currentHour){
+                row.addClass('past')
+            }
+            if(parseInt(time) === currentHour){
                 row.addClass('current');
                 upcoming = true;
-                row.removeClass('past');
-            } 
+            }
         }
     }
     
+    //Creates local storage variable
+    var storage = JSON.parse(localStorage.getItem('planner'));
+
+    //Checks to see if local storage is empty and creates object
+    if (storage === null){
+        storage = {
+            "9AM" : "", "10AM" : "","11AM" : "","12AM" : "","1PM" : "","2PM" : "","3PM" : "","4PM" : "","5PM" : ""
+        }
+    }
+    
+    //For loop creates each row
     for(key in storage){
         var data = storage[key];
         createHourRow(key, data);
     }
     
+    //Executes creation of each time block
     function setStorage(id,data){
         storage[id] = data;
         localStorage.setItem('planner', JSON.stringify(storage));
@@ -68,6 +69,7 @@ $( document ).ready(function() {
     
     // Save button
     $('.saveTime').on("click", function(e){
+        event.preventDefault();
         var selectionID = this.parentElement.id;
         var selectionData = this.parentElement.querySelectorAll('textarea')[0].value;
         setStorage(selectionID, selectionData);
